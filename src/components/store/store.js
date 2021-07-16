5a3e19e7082676caf5a4cba10162f655d16d28a1
Jirawat Nuanjun <86031118+jirawat777@users.noreply.github.com>
2021-07-16 10:54:51 +0700

    add redux persist

---
+++ src/components/store/store.js
@@ -0,0 +1,25 @@
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
+import rootSaga from '../saga';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { rootReducer } from '../reducers/rootReducer';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart', 'auth']
+}
+
+const sagaMiddleware = createSagaMiddleware()
+const middlewares = [thunk, sagaMiddleware]
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export default () => {
+    let store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(...middlewares)))
+    let persistor = persistStore(store)
+    sagaMiddleware.run(rootSaga)
+    return { store, persistor }
+}
--- src/index.js
+++ src/index.js
@@ -3,30 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import rootSaga from './components/saga';
-
 import 'semantic-ui-css/semantic.min.css'
-import { createStore, applyMiddleware } from 'redux'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import createSagaMiddleware from 'redux-saga'
-import { rootReducer } from './components/reducers/rootReducer'
-import thunk from 'redux-thunk'
-const sagaMiddleware = createSagaMiddleware()
-const middlewares = [thunk,sagaMiddleware]
+import { PersistGate } from 'redux-persist/integration/react'
+import configureStore from './components/store/store'
+const { store, persistor } = configureStore()
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)))
-
-sagaMiddleware.run(rootSaga)
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode >,
   document.getElementById('root')
 );
 
